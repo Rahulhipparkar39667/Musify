6819a625e4becea2b98916329d3ebe1f4efc7fd2
Rahul Hipparkar <you@example.com>
2020-04-14 18:41:36 +0530

    Updates

---
+++ precache-manifest.4f730abd5b3bdb9965e86824205ce976.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6803de362ff96e4865da52efd28f39b4",
+    "url": "/Musify/index.html"
+  },
+  {
+    "revision": "8340db44fa8e95b93acd",
+    "url": "/Musify/static/css/2.87b6737d.chunk.css"
+  },
+  {
+    "revision": "e96447a84d19f9bcab62",
+    "url": "/Musify/static/css/main.ed5e86b6.chunk.css"
+  },
+  {
+    "revision": "8340db44fa8e95b93acd",
+    "url": "/Musify/static/js/2.e3c6da97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Musify/static/js/2.e3c6da97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e96447a84d19f9bcab62",
+    "url": "/Musify/static/js/main.c167583f.chunk.js"
+  },
+  {
+    "revision": "707d43fd0566c2d5da3c",
+    "url": "/Musify/static/js/runtime-main.e885fb30.js"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/Musify/static/media/economica-bold.82620571.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Musify/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Musify/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Musify/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Musify/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Musify/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/Musify/static/media/roboto-condensed.light.cf4449c1.ttf"
+  }
+]);
